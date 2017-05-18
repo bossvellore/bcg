300369f46f575237966184138ff95dac7e9788c8
neethia <neethia87@gmail.com>
2017-05-18 16:17:49 +0530

    sdfgsdfg

--- Novice/MainActivity.cs
+++ Novice/MainActivity.cs
@@ -9,115 +9,251 @@ using Android.Graphics;
 using Android.Support.V4.Print;
 using Android.Icu.Text;
 using Java.Util;
+using Android.Print;
+using Android.Runtime;
+using System.IO;
+using Android.Print.Pdf;
+using Android.Views;
+using Android.PrintServices;
+using Android.Graphics.Pdf;
 
 namespace Novice
 {
     [Activity(Label = "Label Printer", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-		ImageView previewImage;
-		Bitmap barcode;
-		EditText date;
-		EditText itemNumber;
-		EditText caseCount;
-		EditText skidNumber;
-		EditText parallelizer;
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
+        ImageView previewImage;
+        Bitmap barcode;
+        EditText date;
+        EditText itemNumber;
+        EditText caseCount;
+        EditText skidNumber;
+        EditText parallelizer;
+        TextView itemId;
+        TextView caseCountId;
+        TextView skidCountId;
+        TextView prodDateId;
+        TextView palletizersinitialId;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
-			previewImage = FindViewById<ImageView>(Resource.Id.previewImg);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            previewImage = FindViewById<ImageView>(Resource.Id.previewImg);
 
 
-			itemNumber = FindViewById<EditText>(Resource.Id.itemNumber);
-			caseCount = FindViewById<EditText>(Resource.Id.caseCount);
-			skidNumber = FindViewById<EditText>(Resource.Id.skidNumber);
-			parallelizer = FindViewById<EditText>(Resource.Id.parallelizer);
+            itemNumber = FindViewById<EditText>(Resource.Id.itemNumber);
+            caseCount = FindViewById<EditText>(Resource.Id.caseCount);
+            skidNumber = FindViewById<EditText>(Resource.Id.skidNumber);
+            parallelizer = FindViewById<EditText>(Resource.Id.parallelizer);
 
-			date = FindViewById<EditText>(Resource.Id.date);
-			//var currentTime = new Date();
-			//var format = new SimpleDateFormat("MMM dd,yyyy hh:mm a");
-			//string dateString = format.Format(currentTime);
-			//date.Text = dateString;
-			date.Text = DateTime.Now.ToString("yy-MM-dd hh:mm");
-			date.Click += (sender, e) =>
-			{
-				new DatePickerDialog(this, DatePickerChangeListener, DateTime.Now.Year, 
-				                     DateTime.Now.Month, DateTime.Now.Day).Show();
-			};
+            //table textview
+            itemId = FindViewById<TextView>(Resource.Id.itemId);
+            caseCountId = FindViewById<TextView>(Resource.Id.caseCountId);
+            skidCountId = FindViewById<TextView>(Resource.Id.skidCountId);
+            prodDateId = FindViewById<TextView>(Resource.Id.prodDateId);
+            palletizersinitialId = FindViewById<TextView>(Resource.Id.palletizersinitialId);
 
-			Button printBtn = FindViewById<Button>(Resource.Id.printBtn);
-			printBtn.Click += delegate
-			{
-				string text = this.GetBarcodeText();
-				GenerateBarcode(text);
-				PrintBarcode();
-			};
+            date = FindViewById<EditText>(Resource.Id.date);
+            //var currentTime = new Date();
+            //var format = new SimpleDateFormat("MMM dd,yyyy hh:mm a");
+            //string dateString = format.Format(currentTime);
+            //date.Text = dateString;
+            date.Text = DateTime.Now.ToString("MM/dd/yy");
+            date.Click += (sender, e) =>
+            {
+                new DatePickerDialog(this, DatePickerChangeListener, DateTime.Now.Year,
+                                     DateTime.Now.Month, DateTime.Now.Day).Show();
+            };
 
-			Button previewBtn = FindViewById<Button>(Resource.Id.previewBtn);
-			previewBtn.Click += (sender, e) =>
-			{
-				string text = this.GetBarcodeText();
-				this.GenerateBarcode(text);
-				previewImage.SetImageBitmap(barcode);
-			};
-		}
+            Button printBtn = FindViewById<Button>(Resource.Id.printBtn);
+            printBtn.Click += delegate
+            {
+                string text = this.GetBarcodeText();
+                GenerateBarcode(text);
+                PrintBarcode();
+            };
 
-		void DatePickerChangeListener(object sender, DatePickerDialog.DateSetEventArgs e)
-		{
-			//throw new NotImplementedException();
-			date.Text = e.Date.ToString("yy-MM-dd hh:mm");
-		}
+            Button previewBtn = FindViewById<Button>(Resource.Id.previewBtn);
+            previewBtn.Click += (sender, e) =>
+            {
 
-		private string GetBarcodeText()
-		{
-			return date.Text + itemNumber.Text + caseCount.Text + skidNumber.Text + parallelizer.Text;
-		}
-		private void PrintBarcode()
-		{
-			PrintHelper photoPrinter = new PrintHelper(this);
-			photoPrinter.ScaleMode = PrintHelper.ScaleModeFit;
+
+               itemId.Text= itemNumber.Text;
+                caseCountId.Text=caseCount.Text;
+                skidCountId.Text=skidNumber.Text;
+                prodDateId.Text=date.Text;
+                palletizersinitialId.Text=parallelizer.Text;
+
+                string text = this.GetBarcodeText();
+                this.GenerateBarcode(text);
+                previewImage.SetImageBitmap(barcode);
+            };
+        }
+
+        void DatePickerChangeListener(object sender, DatePickerDialog.DateSetEventArgs e)
+        {
+            //throw new NotImplementedException();
+            date.Text = e.Date.ToString("MM/dd/yy");
+        }
+
+        private string GetBarcodeText()
+        {
+            return date.Text + "  " + itemNumber.Text + "  " + caseCount.Text + "  " + skidNumber.Text + "  " + parallelizer.Text;
+        }
+        private void PrintBarcode()
+        {
+            //  PrintHelper photoPrinter = new PrintHelper(this);
+            //   photoPrinter.ScaleMode = PrintHelper.ScaleModeFit;
             //Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.droids);
-			photoPrinter.PrintBitmap(GetString(Resource.String.ApplicationName), barcode);
-		}
-		private Bitmap GenerateBarcode(string text)
-		{
-			
-			if (text.Trim() == "")
-			{
-				return null;
-			}
-			var barcodeWriter = new ZXing.Mobile.BarcodeWriter();
-			var Options = new ZXing.Common.EncodingOptions
-			{
-				Width = 576,
-				Height = 384,
-				Margin = 5
-			};
-			barcodeWriter.Renderer = new BitmapRenderer();
-			barcodeWriter.Options = Options;
-			barcodeWriter.Format = BarcodeFormat.CODE_128;
-			//   barcodeWriter.Encode(text);
-			barcode = barcodeWriter.Write(text);
-			// g = Graphics.FromImage(barcode);
-			float scale = Resources.DisplayMetrics.Density;
-			Canvas canvas = new Canvas(barcode);
-			// new antialised Paint
-			Paint paint = new Paint(PaintFlags.AntiAlias);
-			paint.SetStyle(Paint.Style.FillAndStroke);
-			paint.TextSize = 12 * scale;
-			Rect bounds = new Rect();
-			paint.GetTextBounds(text, 0, text.Length, bounds);
-						int x = (barcode.Width - bounds.Width()) / 2;
-			int y = (barcode.Height - bounds.Height());
-			paint.Color = new Color(250, 250, 250);
-			canvas.DrawRect(0, y-10, barcode.Width, barcode.Height, paint);
-						paint.Color = new Color(10, 110, 80);
-			canvas.DrawText(text, x, y+bounds.Height()-5, paint);
-			return barcode;
-		}
+            //   photoPrinter.PrintBitmap(GetString(Resource.String.ApplicationName), barcode);
+            string text = this.GetBarcodeText();
+            this.GenerateBarcode(text);
+            previewImage.SetImageBitmap(barcode);
+            PrintBarcodeCustom();
+
+
+        }
+        private void PrintBarcodeCustom()
+        {
+           
+           var printManager = (PrintManager)GetSystemService(Context.PrintService);
+
+            var content = FindViewById<LinearLayout>(Resource.Id.linearLayout1);
+            var printAdapter = new GenericPrintAdapter(this, content);
+
+            printManager.Print("MyPrintJob", printAdapter, null);
+
+
+
+
+        }
+
+
+
+        private Bitmap GenerateBarcode(string text)
+        {
+
+            if (text.Trim() == "")
+            {
+                return null;
+            }
+            var barcodeWriter = new ZXing.Mobile.BarcodeWriter();
+            var Options = new ZXing.Common.EncodingOptions
+            {
+                Width = 576,
+                Height = 384,
+                Margin = 5
+            };
+            barcodeWriter.Renderer = new BitmapRenderer();
+            barcodeWriter.Options = Options;
+            barcodeWriter.Format = BarcodeFormat.CODE_128;
+            //   barcodeWriter.Encode(text);
+            barcode = barcodeWriter.Write(text);
+            // g = Graphics.FromImage(barcode);
+            float scale = Resources.DisplayMetrics.Density;
+            Canvas canvas = new Canvas(barcode);
+            // new antialised Paint
+            Paint paint = new Paint(PaintFlags.AntiAlias);
+            paint.SetStyle(Paint.Style.FillAndStroke);
+            paint.TextSize = 12 * scale;
+            Rect bounds = new Rect();
+            paint.GetTextBounds(text, 0, text.Length, bounds);
+            int x = (barcode.Width - bounds.Width()) / 2;
+            int y = (barcode.Height - bounds.Height());
+            paint.Color = new Color(250, 250, 250);
+            canvas.DrawRect(0, y - 10, barcode.Width, barcode.Height, paint);
+            paint.Color = new Color(10, 110, 80);
+            canvas.DrawText(text, x, y + bounds.Height() - 5, paint);
+            return barcode;
+        }
+
+
+        public class GenericPrintAdapter : PrintDocumentAdapter
+        {
+            View view;
+            Context context;
+            private int totalPages;
+            PrintedPdfDocument document;
+            float scale;
+            public GenericPrintAdapter(Context context, View view)
+            {
+                this.view = view;
+                this.context = context;
+                totalPages = 3;
+            }
+     
+            public override void OnLayout(PrintAttributes oldAttributes, PrintAttributes newAttributes,
+                                           CancellationSignal cancellationSignal, LayoutResultCallback callback, Bundle extras)
+            {
+                document = new PrintedPdfDocument(context, newAttributes);
+                CalculateScale(newAttributes);
+                if (totalPages > 0)
+                {
+                    PrintDocumentInfo printInfo = new PrintDocumentInfo
+                        .Builder("MyPrint.pdf")
+                        .SetContentType(PrintContentType.Document)
+                        .SetPageCount(totalPages)
+                        .Build();
+
+                    callback.OnLayoutFinished(printInfo, true);
+                }
+                {
+                    callback.OnLayoutFailed("Page count calculation failed.");
+                }
+            }
+            void CalculateScale(PrintAttributes newAttributes)
+            {
+                 int dpi = Math.Max(newAttributes.GetResolution().HorizontalDpi, newAttributes.GetResolution().VerticalDpi);
+
+                 int leftMargin = (int)(dpi * (float)newAttributes.MinMargins.LeftMils / 2500);
+                int rightMargin = (int)(dpi * (float)newAttributes.MinMargins.RightMils / 2500);
+                 int topMargin = (int)(dpi * (float)newAttributes.MinMargins.TopMils / 2500);
+                 int bottomMargin = (int)(dpi * (float)newAttributes.MinMargins.BottomMils / 2500);
+
+                int w = (int)(dpi * (float)newAttributes.GetMediaSize().WidthMils / 2500) - leftMargin - rightMargin;
+                int h = (int)(dpi * (float)newAttributes.GetMediaSize().HeightMils / 2500) - topMargin - bottomMargin;
+
+                scale = Math.Min((float)document.PageContentRect.Width() / w, (float)document.PageContentRect.Height() / h);
+            }
+
+            public override void OnWrite(PageRange[] pages, ParcelFileDescriptor destination,
+                                          CancellationSignal cancellationSignal, WriteResultCallback callback)
+            { 
+
+                for (int i = 0; i < totalPages; i++) {
+                    PrintedPdfDocument.Page page = document.StartPage(i);
+                    
+                    page.Canvas.Scale(scale, scale);
+                    view.Top =50;
+                    view.Draw(page.Canvas);
+                    document.FinishPage(page);
+                    WritePrintedPdfDoc(destination);
+
+                }
+                document.Close();
+                document.Dispose();
+
+
+                callback.OnWriteFinished(pages);
+            }
+
+            void WritePrintedPdfDoc(ParcelFileDescriptor destination)
+            {
+                var javaStream = new Java.IO.FileOutputStream(destination.FileDescriptor);
+                var osi = new OutputStreamInvoker(javaStream);
+                using (var mem = new MemoryStream())
+                {
+                    document.WriteTo(mem);
+                    var bytes = mem.ToArray();
+                    osi.Write(bytes, 0, bytes.Length);
+                }
+            }
+         
+        }
+
     }
+
 }
 
---
+++ Novice/obj/Debug/android/src/md5641421c50131b0c7b266d14a2ae8f082/ImageActivity.java
@@ -0,0 +1,56 @@
+package md5641421c50131b0c7b266d14a2ae8f082;
+
+
+public class ImageActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onOptionsItemSelected:(Landroid/view/MenuItem;)Z:GetOnOptionsItemSelected_Landroid_view_MenuItem_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Novice.ImageActivity, Novice, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", ImageActivity.class, __md_methods);
+	}
+
+
+	public ImageActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == ImageActivity.class)
+			mono.android.TypeManager.Activate ("Novice.ImageActivity, Novice, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onOptionsItemSelected (android.view.MenuItem p0)
+	{
+		return n_onOptionsItemSelected (p0);
+	}
+
+	private native boolean n_onOptionsItemSelected (android.view.MenuItem p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
